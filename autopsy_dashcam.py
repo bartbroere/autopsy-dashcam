ff3ca8d322d23afa6196e3eabf4b6eae3d5b8c9d
Bart Broere <b.broere@nfi.nl>
2020-10-06 22:48:25 +0200

    Fix types and logic

--- autopsy_dashcam.py
+++ autopsy_dashcam.py
@@ -83,10 +83,12 @@ if __name__ == '__main__':
             if mp4.type == b'free':
                 try:
                     data = dict(mp4.__getstate__())['data']
-                    gps_data = data[data.find(b'gps ['):data.find(b'\n\n\x00')]
+                    gps_data = data[data.find(b'gps [') + 4:data.find(b'\n\n\x00')]
                     gps_data = gps_data.split(b'\n\n')
                     gps_out = []
+                    previous_unix_ms = 0
                     for line in gps_data:
+                        line = line.decode('utf8')
                         unix_ms = re.findall(r'(?!\[)[0-9]*(?=\])', line)[0]
                         try:
                             parsed_nmea = pynmea2.parse(line.split(']')[-1])
@@ -94,10 +96,11 @@ if __name__ == '__main__':
                                 if unix_ms != previous_unix_ms:
                                     lat = parsed_nmea.latitude
                                     lon = parsed_nmea.longitude
-                                    gps_out.append([unix_ms, lat, lon])
+                                    gps_out.append([int(unix_ms), lat, lon])
                                     previous_unix_ms = unix_ms
                         except:
                             continue
                     print(json.dumps(gps_out))
+                    quit(0)
                 except:
                     continue
