25a5bc3a15a639867f441dd82762f8c35257eb18
Bart Broere <b.broere@nfi.nl>
2020-10-06 23:23:10 +0200

    Fix exit code

--- autopsy_dashcam.py
+++ autopsy_dashcam.py
@@ -2,6 +2,7 @@ import inspect
 import platform
 import subprocess
 import json
+import sys
 
 if platform.system() == 'Java':  # Jython runtime imports
     from java.util.logging import Level
@@ -90,6 +91,7 @@ if platform.system() == 'Java':  # Jython runtime imports
 
 # Python 3 code from here on
 if __name__ == '__main__':
+    import construct
     from pymp4 import parser
     import re
     import pynmea2
@@ -97,28 +99,33 @@ if __name__ == '__main__':
     # TODO open from stdin
     with open('C:\\Users\TEMP\Downloads\\vid.mp4', 'rb') as f:
         while True:
-            mp4 = parser.Box.parse_stream(f)
-            if mp4.type == b'free':
-                try:
-                    data = dict(mp4.__getstate__())['data']
-                    gps_data = data[data.find(b'gps [') + 4:data.find(b'\n\n\x00')]
-                    gps_data = gps_data.split(b'\n\n')
-                    gps_out = []
-                    previous_unix_ms = 0
-                    for line in gps_data:
-                        line = line.decode('utf8')
-                        unix_ms = re.findall(r'(?!\[)[0-9]*(?=\])', line)[0]
-                        try:
-                            parsed_nmea = pynmea2.parse(line.split(']')[-1])
-                            if hasattr(parsed_nmea, 'latitude'):
-                                if unix_ms != previous_unix_ms:
-                                    lat = parsed_nmea.latitude
-                                    lon = parsed_nmea.longitude
-                                    gps_out.append([int(unix_ms), lat, lon])
-                                    previous_unix_ms = unix_ms
-                        except:
-                            continue
-                    print(json.dumps(gps_out))
-                    quit(0)
-                except:
-                    continue
+            try:
+                mp4 = parser.Box.parse_stream(f)
+                # mp4 = parser.Box.parse_stream(sys.stdin)
+                if mp4.type == b'free':
+                    try:
+                        data = dict(mp4.__getstate__())['data']
+                        gps_data = data[data.find(b'gps [') + 4:data.find(b'\n\n\x00')]
+                        gps_data = gps_data.split(b'\n\n')
+                        gps_out = []
+                        previous_unix_ms = 0
+                        for line in gps_data:
+                            line = line.decode('utf8')
+                            unix_ms = re.findall(r'(?!\[)[0-9]*(?=\])', line)[0]
+                            try:
+                                parsed_nmea = pynmea2.parse(line.split(']')[-1])
+                                if hasattr(parsed_nmea, 'latitude'):
+                                    if unix_ms != previous_unix_ms:
+                                        lat = parsed_nmea.latitude
+                                        lon = parsed_nmea.longitude
+                                        gps_out.append([int(unix_ms), lat, lon])
+                                        previous_unix_ms = unix_ms
+                            except:
+                                continue
+                        print(json.dumps(gps_out))
+                        quit(0)
+                    except:
+                        continue
+            except construct.core.ConstError:
+                break
+
